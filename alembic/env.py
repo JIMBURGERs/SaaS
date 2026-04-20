2c8818f9694c8acaf4b91d70c9a48f7cbfe5c82f
JIMBURGERs <mrhong0423@gmail.com>
2026-04-20 14:59:49 -0400

    Ran migration to update new db tables and columns

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,11 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-from SaaS.app.utils.config import settings
+from app.utils.config import settings
 from app.db import Base
 from app.models.users import User
+from app.models.email_verification_codes import EmailVerificationCode
+from app.models.phone_verification_codes import PhoneVerificationCode
 
 config = context.config
 
